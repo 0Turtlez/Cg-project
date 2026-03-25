6ec92da4abcffc2359fd5316102d9a287591e594
0Turtlez <misterzd@icloud.com>
2026-03-25 15:36:03 -0500

    Added ferries wheel struct and animation code

--- main.cpp
+++ main.cpp
@@ -46,8 +46,8 @@ struct RecSize // Rectangle Size Struct
 const Color WHITE = { 1.0f, 1.0f, 1.0f };
 const Color BLACK = { 0.0f, 0.0f, 0.0f };
 const Color RED = { 1.0f, 0.0f, 0.0f };
-const Color BLUE = { 0.0f, 1.0f, 0.0f };
-const Color GREEN = { 0.0f, 0.0f, 1.0f };
+const Color BLUE = { 0.0f, 0.0f, 1.0f };
+const Color GREEN = { 0.0f, 1.0f, 0.0f };
 
 // -------------- Ease Struct Creation --------------- //
 
@@ -111,7 +111,7 @@ void drawBasicShape(Point pos, float radius, int lines, Color color = WHITE)
 void drawRectangle(RecSize size, Color color = WHITE)
 {
     glBegin(GL_QUADS);
-    glColor4f(color.r, color.b, color.g, color.a);
+    glColor4f(color.r, color.g, color.b, color.a);
     glVertex2f(size.upperLeft.x, size.upperLeft.y);
     glVertex2f(size.lowerLeft.x, size.lowerLeft.y);
     glVertex2f(size.lowerRight.x, size.lowerRight.y);
@@ -152,13 +152,64 @@ void Ground(Point pos)
     drawRectangle(recSize, GREEN);
 }
 
+
+// ------ Ferris Wheel -------
+struct FerrisWheel {
+    Point center;
+    float radius;
+    float position;
+    float rotation;
+
+    int numCars;
+    Color color;
+
+    FerrisWheel(Point _p, float _r, int _cars, Color _c) :
+        center(_p), radius(_r), numCars(_cars), color(_c) {}
+
+    void rotate(float speed) {
+        rotation += speed;
+        if (rotation > 360.0f) rotation -= 360.0f;
+    }
+};
+
+// TODO: Isolate cars and Wheel draw
+void drawFerris(FerrisWheel &fw) {
+    // Draw Wheel
+    drawBasicShape(fw.center, fw.radius, 100, fw.color);
+    drawBasicShape(fw.center, 290, 30, WHITE);
+
+    // Draw spokes and cars
+    for (int i = 0; i < fw.numCars; i++) {
+        float angle = (2.0f *  PI * i / fw.numCars) + (fw.rotation * PI / 180.0f);
+
+        float carX = fw.center.x + fw.radius * cos(angle);
+        float carY = fw.center.y + fw.radius * sin(angle);
+
+        glBegin(GL_LINES);
+            glColor4f(BLACK.r, BLACK.g, BLACK.b, 1.0);
+            glVertex2f(fw.center.x, fw.center.y);
+            glVertex2f(carX, carY);
+        glEnd();
+
+        drawBasicShape(NewPos(carX, carY), 30, 4, RED);
+    }
+
+}
+
+FerrisWheel ferris(NewPos(0, 0), 300.0f, 8, BLUE);
+
+void animateFerris() {
+    ferris.rotate(1.0f);
+}
+
 // ------ Display Function --> Represent Draw Order ------ //
 void display() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Create Ground
     Ground(NewPos(0, -520));
-
+    //Ferris Wheel
+    drawFerris(ferris);
     // Create Clouds
     CreateCloud(NewPos(700, 400));
     CreateCloud(NewPos(-600, 500));
@@ -176,6 +227,20 @@ void init() {
     gluOrtho2D(-screenWidth, screenWidth, -screenHeight, screenHeight);
 }
 
+// Will run code every 60 FPS
+void update() {
+    animateFerris();
+}
+
+// Timer func for animation timing
+void timer(int value) {
+    update();
+    // Triggers redraw
+    glutPostRedisplay();
+    // Waits 16ms per update
+    glutTimerFunc(16, timer, value);
+}
+
 int main(int argc, char** argv) {
 
     // Initialize Display
@@ -187,7 +252,7 @@ int main(int argc, char** argv) {
     // Run Graphics
     init();
     glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
     glutMainLoop();
-
     return 0;
 }
